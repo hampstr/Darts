221976299fc1c2ed3f8f31d09ff9a2b49fb137c3
Radee khan <khanradee318@gmail.com>
2024-09-14 19:01:18 -0400

    Added Tutorial and changed the colors

--- sketch.js
+++ sketch.js
@@ -1,20 +1,42 @@
 
 let canvas
+let autoStartGame = true
+let moveMode = "vertical"
+
+
 
 
 function setup() {
   canvas = createCanvas(700, 700);
+  if (!autoStartGame) {
+    confirm("Start?")
+  }
 }
 
 function draw() {
   background(255);
   canvas.position(windowWidth/2 - width/2, windowHeight/2 - height/2);
-
-  fill(255, 0, 0)
   strokeWeight(10)
+
+
+  // fill(200, 0, 0)
+  fill(211, 145, 229)
   circle(width/2, height/2, 600)
-  fill(255, 255, 0)
+
+
+  // fill(255, 222, 89)
+  fill(154, 98, 170)
   circle(width/2, height/2, 350)
-  fill(0, 255, 0)
+
+
+  // fill(94, 160, 68)
+  fill(117, 57, 134)
   circle(width/2, height/2, 100)
+
+
+}
+
+
+function mouseReleased() {
+
 }
